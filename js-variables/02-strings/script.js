5886a5dde179dcee802dec640f27b601e638d408
francescovalenti <francescovalenti83@outlook.it>
2025-02-13 22:01:04 +0100

    feat:2

--- js-variables/02-strings/script.js
+++ js-variables/02-strings/script.js
@@ -8,7 +8,11 @@ console.log(myname);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number = "32";
+console.log(number);
+let text = "ciao";
+console.log(text);
+let bool = "true";
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
